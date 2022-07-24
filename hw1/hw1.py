30d22c8e39ffb4ad59df5793a4b0d67a04fd3901
ivanwang0207 <ivanwang0207@caece.net>
2022-07-24 16:15:03 +0800

    revise output path

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -8,108 +8,96 @@ file_full_path = os.path.realpath(__file__)
 file_dir = os.path.dirname(file_full_path)
 
 file_name = "lena.bmp"
-result_name = "result.jpg"
-
 img = Image.open(os.path.join(file_dir, file_name))
-width, height = img.size
-#print(img.size)
-#img.show()
+w, h = img.size # (512, 512)
 
 def upside_down(img):
 	result = Image.new(img.mode, img.size)
-	for y in range(height//2): # 0-255
-		for x in range(width): # 0-511
+	for y in range(h//2): # 0-255
+		for x in range(w
+	): # 0-511
 			up = img.getpixel((x, y))
-			down = img.getpixel((x, height - 1 - y)) # 512 - 1 - y :511,510,509.....,256
+			down = img.getpixel((x, h - 1 - y)) 
 		
-			result.putpixel((x, height - 1 - y), up)
+			result.putpixel((x, h - 1 - y), up)
 			result.putpixel((x, y), down)
 
-	result.show()
-	return result
+	result.save(os.path.join(file_dir, "lena_upside_down.bmp"))
+	#result.show()
 
 def right_side_left(img):
 	result = Image.new(img.mode, img.size)
-	for y in range(height): # 0-511
-		for x in range(width//2): # 0-255
+	for y in range(h): # 0-511
+		for x in range(w
+	//2): # 0-255
 			left = img.getpixel((x, y))
-			right = img.getpixel((width - 1 - x, y))
+			right = img.getpixel((w
+		 - 1 - x, y))
 			
-			result.putpixel((width - 1 - x, y), left)
+			result.putpixel((w
+		 - 1 - x, y), left)
 			result.putpixel((x, y), right)
 
-	result.show()
-	return result
+	result.save(os.path.join(file_dir, "lena_right_side_left.bmp"))
+	#result.show()
 
 def diagonally_mirrored(img):
 	result = Image.new(img.mode, img.size)
-	for y in range(height):
-		for x in range(width - y):
+	for y in range(h):
+		for x in range(w
+	 - y):
 			a = img.getpixel((x, y))
-			b = img.getpixel((width - 1 - x, height - 1 - y))
+			b = img.getpixel((w
+		 - 1 - x, h - 1 - y))
 			
-			result.putpixel((width - 1 - x, height - 1 - y), a)
+			result.putpixel((w
+		 - 1 - x, h - 1 - y), a)
 			result.putpixel((x, y), b)
 
-	result.show()
-	return result
+	result.save(os.path.join(file_dir, "lena_diagonally_flip.bmp"))
+	#result.show()
 
 def rotate(img):
 	result = img.rotate(45)
-	result.show()
-	return result
+ 
+	result.save(os.path.join(file_dir, "lena_rotate45.bmp"))
+	#result.show()
 
 def shrink(img):
-	x, y = int(width/2) ,int(height/2)
+	x, y = int(w
+/2) ,int(h/2)
 	result = img.resize((x,y))
-	result.show()
-	return result
+ 
+	result.save(os.path.join(file_dir, "lena_shrink.bmp"))
+	#result.show()
 
 def binarize(img):
 	im = np.array(img.convert('L'))
 	th = 128
 	im_bin_128 = (im > th) * 255
-	# print(im_bin_128)
 	result = Image.fromarray(np.uint8(im_bin_128))
-	result.show()
-	return result
-	
-def upside_down2(img):
-	img_new = np.zeros(img.shape)
-	
-	# for y in range(height): 
-	# 	temp = img[y,:]
-	# 	img_new[height-y-1,:] = temp
-	# cv2.imwrite('test.jpg',img_new)
 
+	result.save(os.path.join(file_dir, "lena_binarize.bmp"))
+	#result.show()
+	
+def upside_down_opencv(img):
 	img = img.tolist()
-	for y in range(height//2):
+	for y in range(h//2):
 		temp = img[y]
-		img[y] = img[height-y-1]
-		img[height-y-1] = temp		
+		img[y] = img[h-y-1]
+		img[h-y-1] = temp		
 	img = np.asarray(img)
-	cv2.imwrite(os.path.join(file_dir, result_name),img)
+ 
+	cv2.imwrite(os.path.join(file_dir, "lena_upside_down.bmp"),img)
 
 if __name__ == '__main__':
-
-# arr = np.array([[1,2,3],
-# 				[4,5,6],
-# 				[7,8,9]])
-
-# temp = arr[0]
-# print('First temp = arr[0] =  ',temp)
-# arr[0] = arr[2]
-# print('If arr[0] and temp share same memory: ',np.may_share_memory(arr[0], temp))
-# print('After arr[0] asign to arr[2], temp = arr[0] =  ',temp)
-# arr[2] = temp
-# print('Final arr = \n',arr)
-
-	img2 = cv2.imread(os.path.join(file_dir, file_name))
-	upside_down2(img2)
-
-# upside_down(img).save("lena_upside_down.bmp")
-# right_side_left(img).save("lena_right_side_left.bmp")
-# diagonally_mirrored(img).save("lena_diagonally_flip.bmp")
-# rotate(img).save("lena_rotate45.bmp")
-# shrink(img).save("lena_shrink.bmp")
-# binarize(img).save("lena_binarize.bmp")
+	
+	upside_down(img)
+	right_side_left(img)
+	diagonally_mirrored(img)
+	rotate(img)
+	shrink(img)
+	binarize(img)
+ 
+	#img_arr = cv2.imread(os.path.join(file_dir, file_name))
+	#upside_down_opencv(img_arr)
